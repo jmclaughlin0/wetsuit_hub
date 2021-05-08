440a05d3f1b866c42003fbbb89b7d6649dfaa6e1
Joe McLaughlin <JoeMcLaughlin@Joes-MacBook-Air.local>
2021-05-08 14:04:50 +0100

    Create React App and Spring

---
+++ wetsuit_hub-web/src/main/java/com/nyxgroup/wetsuit_hubweb/WetsuitHubWebApplication.java
@@ -0,0 +1,13 @@
+package com.nyxgroup.wetsuit_hubweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetsuitHubWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetsuitHubWebApplication.class, args);
+	}
+
+}
---
+++ wetsuit_hub-web/src/test/java/com/nyxgroup/wetsuit_hubweb/WetsuitHubWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyxgroup.wetsuit_hubweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetsuitHubWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
